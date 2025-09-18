55d4e57b24a6955a5ed346505e623040df3aff46
Your Name <you@example.com>
2025-09-17 20:54:23 -0500

    Primer commit, tomando las fotos desde el file system del dispositivo

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,13 +1,30 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';     
+import { IonicModule } from '@ionic/angular';       
+import { Camera, CameraResultType, CameraSource } from '@capacitor/camera';
 
 @Component({
   selector: 'app-tab2',
   templateUrl: 'tab2.page.html',
   styleUrls: ['tab2.page.scss'],
-  standalone: false,
+  standalone: true,
+  imports: [CommonModule, IonicModule]   
 })
 export class Tab2Page {
+  photos: string[] = [];
 
   constructor() {}
 
+  async takePhoto() {
+    const image = await Camera.getPhoto({
+      quality: 90,
+      allowEditing: false,
+      resultType: CameraResultType.DataUrl, // usamos DataUrl para mostrar fácil en <img>
+      source: CameraSource.Camera
+    });
+
+    if (image && image.dataUrl) {
+      this.photos.unshift(image.dataUrl); // agrega la foto al inicio del array
+    }
+  }
 }
